528ab8ed50fa40d3609ffd24668bf9fca73bb4af
w1esther <w1mariaesther@gmail.com>
2025-08-10 10:35:43 -0300

    analise de primos
    este programa analisa de forma simples se um número é ou não um número primo

---
+++ primos.py
@@ -0,0 +1,15 @@
+x = int(input('entre com um valor e direi se é primo ou não: '))
+
+lista = []
+
+for n in range(1, x+1):
+    if x % n == 0:
+        lista.append(n)
+
+elementos_lista = len(lista)
+
+if elementos_lista == 2:
+    print(f'{x} é um número primo!')
+else:
+    print(f'{x} não é um número primo!')
+
